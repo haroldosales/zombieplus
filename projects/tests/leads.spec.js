11bb55643c31bc299c10f83c40b330f8f163828a
haroldosales <haroldognu@gmail.com>
2024-12-23 20:33:16 -0300

    new build web

--- projects/tests/leads.spec.js
+++ projects/tests/leads.spec.js
@@ -46,7 +46,7 @@ test("deve cadastra com email errado ", async ({ page }) => {
   await page.waitForTimeout(1000);
 });
 
-test("deve validar campo nome obrigatorio ", async ({ page }) => {
+test("não deve validar campo email nao preenchido ", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   //await page.click('//button[co( )="Aperte o play... se tiver coragem"]')
 
@@ -56,28 +56,32 @@ test("deve validar campo nome obrigatorio ", async ({ page }) => {
     "Fila de espera"
   );
 
-  await page.locator("input[name=email]").fill("test@esa.com");
-
-  page.getByRole("button", { name: "Quero entrar na fila" }).click();
-
-  const error = "Campo obrigatório";
-
-  await expect(page.locator(".alert")).toHaveText(error);
-
-  await page.waitForTimeout(1000);
-});
-
-test("deve validar campo email obrigatorio ", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  //await page.click('//button[co( )="Aperte o play... se tiver coragem"]')
-
-  page.getByRole("button", { name: /Aperte o play/ }).click();
-
-  expect(page.getByTestId("modal").getByRole("heading")).toHaveText(
-    "Fila de espera"
-  );
 
   await page.locator("#name").fill("silva silva");
+  page.getByRole("button", { name: "Quero entrar na fila" }).click();
+
+  const error = "Campo obrigatório";
+
+  await expect(page.locator(".alert")).toHaveText(error);
+
+
+
+  await page.waitForTimeout(1000);
+});
+
+test("não deve validar campo nome nao preenchido ", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  //await page.click('//button[co( )="Aperte o play... se tiver coragem"]')
+
+  page.getByRole("button", { name: /Aperte o play/ }).click();
+
+  expect(page.getByTestId("modal").getByRole("heading")).toHaveText(
+    "Fila de espera"
+  );
+
+    await page.locator("input[name=email]").fill("tes@tesa.com");
+
+
 
   page.getByRole("button", { name: "Quero entrar na fila" }).click();
 
@@ -85,5 +89,36 @@ test("deve validar campo email obrigatorio ", async ({ page }) => {
 
   await expect(page.locator(".alert")).toHaveText(error);
 
+
+
   await page.waitForTimeout(1000);
 });
+
+
+test("não deve validar campo nao preenchido ", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  //await page.click('//button[co( )="Aperte o play... se tiver coragem"]')
+
+  page.getByRole("button", { name: /Aperte o play/ }).click();
+
+  expect(page.getByTestId("modal").getByRole("heading")).toHaveText(
+    "Fila de espera"
+  );
+
+
+
+
+  page.getByRole("button", { name: "Quero entrar na fila" }).click();
+
+  const error = "Campo obrigatório";
+
+  await expect(page.locator(".alert")).toHaveText([
+    "Campo obrigatório",
+   "Campo obrigatório"
+]);
+
+
+
+  await page.waitForTimeout(1000);
+});
+
